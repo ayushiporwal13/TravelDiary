04c9697b512ff0c6b78735b52ce1e7d3a440bbec
Ayushi Porwal <ayushiporwal5@gmail.com>
2024-01-28 16:58:28 -0800

    Postfeed changes

--- src/pages/PostFeed.jsx
+++ src/pages/PostFeed.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../client";
-import Cards from "../components/Cards";
+import Cards from "../components/cards";
 import { Grid, CircularProgress } from "@mui/material";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
